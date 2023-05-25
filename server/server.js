fb4ed96e6cb2ad5f9cd7fc9a1719ef856ca0f83f
Joshua Aquino <joshuamaquino@gmail.com>
2023-05-24 21:25:12 -0400

    added get endpoint

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('pg');
 
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-const pool = new pg. Pool({
+const pool = new pg.Pool({
     host: 'db.bit.io',
     port: 5432,
     database: 'jma503/trial',
@@ -13,6 +13,19 @@ const pool = new pg. Pool({
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+app.get ('/messages', (req, res) => {
+    let query = 'SELECT * FROM "messages";';
+    
+    pool.query(query)
+        .then((result) => {
+            res.status(200).send(result.rows);
+        })
+        .catch((error) => {
+            console.error(error);
+            res.sendStatus(500);
+        })        
+})
 
 app.use(express.static('server/public'));
 app.use(express.json)
