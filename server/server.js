62c6c0df2301272ecf20c6b072517b8172b2dfad
Joshua Aquino <joshuamaquino@gmail.com>
2023-05-24 21:16:46 -0400

    db password

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const express = require('pg');
+
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const pool = new pg. Pool({
+    host: 'db.bit.io',
+    port: 5432,
+    database: 'jma503/trial',
+    user: 'jma503',
+    password: DB_PASSWORD
+})
+
 const app = express();
 const PORT = process.env.PORT || 5001;
 
@@ -13,3 +25,4 @@ app.get('/', (req, res) => {
     console.log('Simple get')
     res.status(200).send();
 });
+
